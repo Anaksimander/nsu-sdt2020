2d79edf2cb0cb464ea220ddb3f7b36486439ab3c
Anaksimander <61375814+Anaksimander@users.noreply.github.com>
2020-10-30 12:49:34 +0700

    Update laba3_1.clj

--- laba3/laba3_1.clj
+++ laba3/laba3_1.clj
@@ -1,11 +1,11 @@
 (ns laba3-1.core
   (:gen-class))
 
-(defn heavy-inc [n]
-  (Thread/sleep 100)
-  (filter even? n))
+(defn heavy-pred [sleep f]
+  (Thread/sleep sleep)
+  f)
  
-(defn main [coll size_bloc]
+(defn separation [coll size_bloc]
   (loop [acc [], new_coll coll]
     (if (>= size_bloc (count new_coll)) (conj acc (take size_bloc new_coll))
       (recur (conj acc (take size_bloc new_coll)) (drop size_bloc new_coll) )
@@ -13,12 +13,16 @@
     )
 )
 
-(defn my-filter [coll size_bloc]
+(defn my-filter [coll size_bloc sleep pred]
   (apply concat
   (doall
    (map deref
         (doall
-         (map #(future (heavy-inc %)) (main coll size_bloc)))))))
+         (map #(future (filter (heavy-pred sleep pred) %)) (separation coll size_bloc)))))))
 
-;(my-filter '((1 2) (3 4) (8)))
-(time (my-filter '(1 2 3 4 5 6) 2))
+
+(defn main []
+  
+  )
+
+(time (my-filter '(1 2 3 4 5 6) 2 100 even?))
