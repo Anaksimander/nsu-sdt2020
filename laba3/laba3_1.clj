9e58e452f5c4f65d9de2fcd12e52fa364f8bf606
Anaksimander <61375814+Anaksimander@users.noreply.github.com>
2020-10-31 11:04:00 +0700

    Update laba3_1.clj

--- laba3/laba3_1.clj
+++ laba3/laba3_1.clj
@@ -7,7 +7,7 @@
     (apply f coll)
     ))
 
-;(def heavy-pred (heavy 100 even?))
+(def heavy-pred (heavy 100 even?))
  
 (defn separation [coll size_bloc]
   (loop [acc [], new_coll coll]
@@ -16,29 +16,31 @@
     )
     ))
 
-(defn my-filter [coll size_bloc pred]
+(defn my-filter [coll size_bloc pred-i]
+  (let [pred (heavy 100 pred-i)]
   (apply concat
   (doall
    (map deref
         (doall
-         (map #(future (doall (filter (heavy 100 pred) %))) (separation coll size_bloc) )
-        )))))  
+         (map #(future (doall (filter pred %))) (separation coll size_bloc) )
+        ))))))  
 
-(defn my-filter-mac [coll size_bloc pred]
+(defn my-filter-mac [coll size_bloc pred-i]
+  (let [pred (heavy 100 pred-i)]
   (->>
    (separation coll size_bloc)
-   (map #(future (doall (filter (heavy 100 pred) %))))
+   (map #(future (doall (filter pred %))))
    (doall)
    (map deref)
    (doall)
-   (apply concat)))
+   (apply concat))))
+
 
 (defn main []
-  ;(time(my-filter '(1 2 3 4 5 6) 2 even?))
+  (time(my-filter '(1 2 3 4 5 6 7 8 9 10) 5 even?))
   ;(time(doall(filter heavy-pred '(1 2 3 4 5 6))))
-  (time (my-filter-mac (take 1000 (range)) 2 even?))
+  ;(time (my-filter-mac '(1 2 3 4 5 6) 2 even?))
+  ;(time (my-filter (take 1000 (range)) 2 even?))
   )
-
-
 
 (main)
